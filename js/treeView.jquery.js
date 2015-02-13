97d897c965ea624f0381248cb0e17a0db1a57f9a
Samarjeet <samarjeet2702@gmail.com>
2015-02-13 16:36:10 +0530

    Generate treeView from data-model

--- js/treeView.jquery.js
+++ js/treeView.jquery.js
@@ -1,15 +1,37 @@
 (function ($){
 	// doesn't work without jquery
 	if (!$) return;
-
 	// treeView
 	function treeView($me) {
 		// collapsable elements i.e. the li with a ul in it
 		var $collapse = $me.find('li>ul').parent();
+		// generate tree from data
+		function generateTree(data, $root) {
+			// create a node from a node object
+			function createNode(nObj) {
+				var li = $('<li>').text(nObj.label);
+				if (nObj.children != undefined && nObj.children.length > 0) {
+					innerList = $('<ul>');
+					for (var i = 0; i < nObj.children.length; i++) {
+						var child = nObj.children[i];
+						innerList.append(createNode(child));
+					};
+					li.append(innerList);
+				}
+				return li;
+			}
+			for (var i = 0; i < data.length; i++) {
+				$root.append(createNode(data[i]));
+			}
+		}
 
 		return {
 			//initialize control
-			init: function () {
+			init: function (data) {
+				if (typeof data == "object"){
+					generateTree(data, $me);
+					$collapse = $me.find('li>ul').parent();
+				}
 				// all the collapsable items which have something
 				$collapse.addClass('contains-items');
 				// user config
@@ -58,7 +80,7 @@
 				return instance[options](args);
 			} else {
 				// no method requested, so initialize
-				instance.init();
+				instance.init(options);
 			}
 		});
 	}
